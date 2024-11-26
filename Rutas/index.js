c6ef6fa60c7e861992f9ab4ec998dd3925fdfd5d
jonie_mirc <jonie.mirc@gmail.com>
2024-11-25 19:30:43 -0600

    Persistencia de datos CRUD completo

--- Rutas/index.js
+++ Rutas/index.js
@@ -52,7 +52,59 @@ app.get('/productos', (req,res)=>{
             res.json(resultado);
         }
     });
-    
+});
+
+app.post('/productos', (req, res)=>{
+    const producto = req.body;
+    const sql ='insert into tr_producto (nombre,precio,codigo_fabricante) values(?,?,?)';
+
+    if(!producto.nombre || !producto.precio || !producto.codigo_fabricante){
+        return res.status(400).json({error:'Todos los campos son obligatorios'});
+    }
+
+    conexion.query(sql, [producto.nombre,producto.precio,producto.codigo_fabricante],(err, resultado)=>{
+        if(err){
+            res.status(500).json({error:'Error al agregar el producto'});
+        }else{
+            res.status(201).json({mesaje:'Producto agregado',codigo: resultado.insertId});
+        }
+    });
+});
+
+app.put('/productos', (req, res)=>{
+    const producto = req.body;
+    const sql ='update tr_producto set nombre=?,precio=?,codigo_fabricante=? where codigo = ?';
+
+    if(!producto.nombre || !producto.precio || !producto.codigo_fabricante || !producto.codigo){
+        return res.status(400).json({error:'Todos los campos son obligatorios'});
+    }
+
+    conexion.query(sql, [producto.nombre,producto.precio,producto.codigo_fabricante, producto.codigo],(err, resultado)=>{
+        if(err){
+            res.status(500).json({error:'Error al actualizar el producto'});
+        }else if(resultado.affectedRows === 0){
+            res.status(404).json({mensaje:'Producto no encontrado'});
+        }else{
+            res.status(200).json({mesaje:'Producto actualizado',producto});
+        }
+    });
+});
+
+app.delete('/productos/:codigo', (req, res)=>{
+    const {codigo} = req.params;
+    const sql ='delete from tr_producto where codigo=?';
+
+    if(!codigo){
+        return res.status(400).json({error:'El parametro código es requerido'});
+    }
+
+    conexion.query(sql, [codigo],(err, resultado)=>{
+        if(err){
+            res.status(500).json({error:'Error al eliminar el producto'});
+        }else{
+            res.status(200).json({mesaje:'Producto eliminado'});
+        }
+    });
 });
 
 app.get('/usuarios-nextid', (req,res)=>{
